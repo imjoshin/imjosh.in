726ee2682049cd75ddfbcb6dd4f35ea0f2a277e4
Josh <jcjohnson77@gmail.com>
2023-05-21 15:44:03 -0400

    Fix ts globals

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,31 +10,30 @@ import { useThemeName } from "../hooks/useThemeName";
 type IndexPageProps = {};
 
 const IndexPage: React.FC<PageProps<IndexPageProps>> = () => {
-    const [theme, setTheme] = useThemeName();
+  const [theme, setTheme] = useThemeName();
 
-    return (
-        <Layout>
-            <div
-                className={styles.textCenter}
-                onClick={() => {
-                    setTheme(theme === "light" ? "dark" : "light");
-                    console.log("test");
-                }}
-            >
-                <StaticImage
-                    src="../images/pixar1.png"
-                    loading="eager"
-                    width={175}
-                    quality={95}
-                    alt=""
-                    style={{ marginBottom: `var(--theme-space-3)` }}
-                />
-                <h1>
-                    Welcome to <b>imjosh.in</b>
-                </h1>
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <div
+        className={styles.textCenter}
+        onClick={() => {
+          setTheme(theme === "light" ? "dark" : "light");
+        }}
+      >
+        <StaticImage
+          src="../images/pixar1.png"
+          loading="eager"
+          width={175}
+          quality={95}
+          alt=""
+          style={{ marginBottom: `var(--theme-space-3)` }}
+        />
+        <h1>
+          Welcome to <b>imjosh.in</b>
+        </h1>
+      </div>
+    </Layout>
+  );
 };
 
 export const Head: HeadFC<IndexPageProps> = () => <SEO />;
