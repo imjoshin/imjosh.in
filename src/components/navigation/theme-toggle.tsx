0374010a6453a7e0be3c201f5a3cad2b6358777f
Josh <jcjohnson77@gmail.com>
2023-05-21 20:42:41 -0400

    Add react-icons for navbar

--- src/components/navigation/theme-toggle.tsx
+++ src/components/navigation/theme-toggle.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useThemeName } from "../../hooks/useThemeName";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 
 interface ThemeToggleProps {
     className?: string;
@@ -8,14 +9,20 @@ interface ThemeToggleProps {
 export const ThemeToggle = ({ className }: ThemeToggleProps) => {
     const [theme, setTheme] = useThemeName();
 
+    const Icon = theme === "light" ? BsFillSunFill : BsFillMoonFill;
+
     return (
         <a
             className={className}
             onClick={() => {
                 setTheme(theme === "light" ? "dark" : "light");
             }}
+            style={{
+                position: 'relative',
+                top: '2px',
+            }}
         >
-            {theme}
+            <Icon />
         </a>
     );
 };
