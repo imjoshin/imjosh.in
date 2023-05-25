b22baa82367b36853daaede987c055c5ff6027dd
Josh <jcjohnson77@gmail.com>
2023-05-24 22:27:08 -0400

    Add external blog posts

---
+++ content/external.ts
@@ -0,0 +1,22 @@
+const articles = [
+    {
+        url: 'https://duo.com/blog/teamwork-makes-the-dream-work-why-we-thrive-on-collaboration',
+        tags: ['external'],
+    },
+    {
+        url: 'https://www.gatsbyjs.com/blog/gatsbys-greatest-hits-in-2021-and-2022/',
+        tags: ['external', 'gatsby'],
+    },
+    {
+        url: 'https://www.gatsbyjs.com/blog/gatsby-slice-api/',
+        tags: ['external', 'gatsby'],
+    },
+    {
+        url: 'https://www.gatsbyjs.com/blog/gatsby-5/',
+        tags: ['external', 'gatsby'],
+    },
+    {
+        url: 'https://www.gatsbyjs.com/blog/gatsby-5-an-upgrade-worth-the-hype/',
+        tags: ['external', 'gatsby'],
+    },
+];
